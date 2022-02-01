2219beab172c752c7aef46ff12fee45f812890b7
Anton Pakhomov <ayupakhomov@gmail.com>
2022-02-01 15:11:30 +0300

    fix

--- api-gateway/main.tf
+++ api-gateway/main.tf
@@ -54,6 +54,10 @@ resource "aws_route53_record" "this" {
   zone_id = var.route53_zone_id
   name    = var.api_gateway_mapped_domain_name
   type    = "A"
-  ttl     = "300"
-  records = [aws_api_gateway_domain_name.this.regional_domain_name]
+  
+  alias {
+    name                   = aws_api_gateway_domain_name.this.regional_domain_name
+    zone_id                = aws_api_gateway_domain_name.this.regional_zone_id
+    evaluate_target_health = true
+  }
 }
