601b502c06a842975ade1c52b5cea11dd9d0eb32
Anton Pakhomov <ayupakhomov@gmail.com>
2022-02-02 13:28:04 +0300

    fix outputs

--- api-gateway/domains/outputs.tf
+++ api-gateway/domains/outputs.tf
@@ -3,6 +3,6 @@ output "api_gateway_domain_name_regional_domain_name" {
 }
 
 output "api_gateway_domain_name_regional_zone_id" {
-  value = try(aws_api_gateway_domain_name.this.zone_id, "")
+  value = try(aws_api_gateway_domain_name.this.regional_zone_id, "")
 }
 
