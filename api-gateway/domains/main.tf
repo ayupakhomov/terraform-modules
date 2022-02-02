7c813c709ca9cb9e6cafed896844f452ed4e37e4
Anton Pakhomov <ayupakhomov@gmail.com>
2022-02-02 13:14:25 +0300

    fixed index

--- api-gateway/domains/main.tf
+++ api-gateway/domains/main.tf
@@ -17,8 +17,8 @@ resource "aws_route53_record" "this" {
   type    = "A"
   
   alias {
-    name                   = aws_api_gateway_domain_name.this[0].regional_domain_name
-    zone_id                = aws_api_gateway_domain_name.this[0].regional_zone_id
+    name                   = aws_api_gateway_domain_name.this.regional_domain_name
+    zone_id                = aws_api_gateway_domain_name.this.regional_zone_id
     evaluate_target_health = true
   }
 }
