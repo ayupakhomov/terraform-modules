615113bade1297fbda8c3b6545f54da3878c5c2c
Anton Pakhomov <ayupakhomov@gmail.com>
2022-02-02 13:33:20 +0300

    fixed module

--- api-gateway/api-gateway/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "api_gateway_domain_name_regional_domain_name" {
-  value = try(aws_api_gateway_domain_name.this[0].regional_domain_name, "")
-}
-
-output "api_gateway_domain_name_regional_zone_id" {
-  value = try(ws_api_gateway_domain_name.this[0].zone_id, "")
-}
-
