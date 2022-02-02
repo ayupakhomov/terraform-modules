15985e46de74a12b963217b890b9953a5142e2ee
Anton Pakhomov <ayupakhomov@gmail.com>
2022-02-02 13:23:24 +0300

    fix domains outputs

---
+++ api-gateway/domains/outputs.tf
@@ -0,0 +1,8 @@
+output "api_gateway_domain_name_regional_domain_name" {
+  value = try(aws_api_gateway_domain_name.this.regional_domain_name, "")
+}
+
+output "api_gateway_domain_name_regional_zone_id" {
+  value = try(aws_api_gateway_domain_name.this.zone_id, "")
+}
+
