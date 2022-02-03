12b09ff20d004159d29eee0bbd9c0661d4162a53
Anton Pakhomov <ayupakhomov@gmail.com>
2022-02-03 13:24:48 +0300

    fix typo

--- api-gateway/api-gateway/main.tf
+++ api-gateway/api-gateway/main.tf
@@ -79,7 +79,7 @@ count = var.create_api_gateway_api_key ? length(var.api_gateway_name) : 0
 }
 
 resource "aws_api_gateway_usage_plan_key" "this" {
-  count = var.var.create_api_gateway_api_key ? 1 : 0
+  count = var.create_api_gateway_api_key ? 1 : 0
   key_id        = aws_api_gateway_api_key.this[count.index].id
   key_type      = "API_KEY"
   usage_plan_id = aws_api_gateway_usage_plan.this[count.index].id
