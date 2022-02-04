a5aa290f9d59a40930846752cce07e64119030e9
Anton Pakhomov <ayupakhomov@gmail.com>
2022-02-04 10:27:20 +0300

    added vars description

--- api-gateway/api-gateway/variables.tf
+++ api-gateway/api-gateway/variables.tf
@@ -5,40 +5,44 @@ variable "create_api_gateway" {
 }
 
 variable "create_api_gateway_usage_plan" {
-  description = "Whether to create API Gateway"
+  description = "Whether to create a Usage Plan for API Gateway"
   type        = bool
   default     = true
 }
 
 variable "create_api_gateway_api_key" {
-  description = "Whether to create API Gateway"
+  description = "Whether to create API Keys"
   type        = bool
   default     = true
 }
 
 variable "create_api_gateway_domain_name" {
-  description = "Whether to create API Gateway"
+  description = "Whether to create a Custom Domain Name for API Gateway"
   type        = bool
   default     = true
 }
 
 variable "api_gateway_name" {
+  description = "Name of API Gateway"
   type    = list
   default = []
 }
 
 variable "api_gateway_defenition_file_name" {
+  description = "Name of the object in S3 Bucket"
   type    = list
   default = null
 }
 
 variable "api_gateway_type" {
+  description = "Type of API Gateway"
   type    = string
   default = "REGIONAL"
 }
 
 
 variable "api_gateway_stage_name" {
+  description = "Name of API Gateway Stage"
   type    = string
   default = ""
 }
@@ -62,45 +66,49 @@ variable "stage_variables" {
 }
 
 variable "api_gateway_mapped_domain_name" {
+  description = "FQDN of API Gateway Custom Domain"
   type    = string
   default = ""
 }
 
 variable "api_gateway_mapped_domain_gateway_type" {
+  description = "Endpoint type for a Custom Domain"
   type    = string
   default = ""
 }
 
 variable "api_gateway_domain_name_certificate_arn" {
+  description = "ARN of TLS Certificate for a Custom Domain"
   type    = string
   default = ""
 }
 
-
-
-
-
 variable "api_gateway_usage_plan_description" {
+  description = "A description for usage plan"
   type    = string
   default = ""
 }
 
 variable "api_gateway_usage_plan_name" {
+  description = "Name for a Usage Plan"
   type    = string
   default = ""
 }
 
 variable "api_gateway_api_key_name" {
+  description = "Name of an API Key"
   type    = string
   default = ""
 }
 
 variable "api_mapping_base_path" {
+  description = "Base path for Custom Domain mapping"
   type    = list
   default = null
 }
 
 variable "bucket_name" {
+  description = "Name of bucket where OAPI definitions are stored"
   type    = string
   default = ""
 }
@@ -129,13 +137,4 @@ variable "api_gateway_usage_plan_throttle_settings_burst_limit" {
 variable "api_gateway_usage_plan_throttle_settings_rate_limit" {
   type    = number
   default = null
-}
-
-## API Keys
-
-variable "api_gateway_api_keys" {
-  type    = map
-  default = {   "key1" = "sjgd934gtu340t"
-   "key2" = "8gdfogakjosgskjsod"
-   "key3" = "dsgdsa8gdskg34dsg"}
 }
